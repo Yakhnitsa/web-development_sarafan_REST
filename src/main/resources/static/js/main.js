d81cffb0f9b289ba18c6eafa1a6e829a0a699216
Yuriy <transpmailbox@gmail.com>
2019-11-19 17:56:45 +0200

    Added API, end of lesson 2

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,6 +1,32 @@
+var messageApi = Vue.resource('/message{/id}');
+
+
+Vue.component('message-row',{
+   template:'<div><i>({{message.id}})</i>  {{message.text}}</div>',
+   props:['message']
+});
+
+
+Vue.component('messages-list', {
+    template:'<div >' +
+                '<message-row v-for="message in messages" :message="message" :key="message.id"></message-row>' +
+            '</div>',
+    props:['messages'],
+});
+
+
 var sarafan = new Vue({
     el: '#sarafan',
+    template:'<messages-list :messages="messages"></messages-list>',
     data:{
-        message: 'Hello user!'
+        message: 'Hello user!',
+        messages:[],
+    },
+    created: function(){
+        messageApi.get().then(result =>
+            result.json().then( data =>
+                data.forEach(message => this.messages.push(message))
+            )
+        )
     }
 })
